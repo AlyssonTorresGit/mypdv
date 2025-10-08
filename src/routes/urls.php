bc24ecf039cc4e63a4c7e273af40c6b2018f8943
Alysson Torres <alysson-b@hotmail.com>
2025-10-08 17:27:48 -0300

    JK

--- src/routes/urls.php
+++ src/routes/urls.php
@@ -9,7 +9,9 @@ use App\Controllers\UsuarioController;
 
 Router::add("GET", "/", BaseController::class, "index");
 Router::add("GET", "/painel-controle", PainelController::class, "index");
-
 Router::add("GET", "/cadastrar-usuario", UsuarioController::class, "index");
 Router::add("POST", "/cadastrar-usuario", UsuarioController::class, "index");
 Router::add("GET", "/listar-usuarios", UsuarioController::class, "listar");
+Router::add("GET", "/deletar-usuarios/{id}", UsuarioController::class, "deleteConfirm");
+Router::add("GET", "/ecluir-usuarios/{id}", UsuarioController::class, "excluir");
+Router::add("GET", "/alterar-status/{id}/{status}", UsuarioController::class, "alterarStatus");
